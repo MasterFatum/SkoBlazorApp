e4be110d79283818b74e1cee452fb790fd15e23b
Иван Панфилов <pi.resonance@hotmail.com>
2021-08-25 10:10:50 +0300

    AddConfirmation

--- SkoBlazorApp/SkoContext.cs
+++ SkoBlazorApp/SkoContext.cs
@@ -24,10 +24,10 @@ namespace SkoBlazorApp
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlServer("Server=172.16.0.22;Database=SOKO;User Id=Sko;Password=Sko123;");
+                optionsBuilder.UseSqlServer("Server=VANHOME;Database=SOKO;Trusted_Connection=True;");
 
                 //Server=172.16.0.22;Database=SOKO;User Id=Sko;Password=Sko123;
-                //Server=WANWORK;Database=SOKO;Trusted_Connection=True;
+                //Server=VANWORK;Database=SOKO;Trusted_Connection=True;
 
             }
         }
